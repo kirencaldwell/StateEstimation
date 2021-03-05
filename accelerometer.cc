0f49202196addf249dfa6fbe0c7ae5dc18051808
Kiren Caldwell <kirencaldwell@gmail.com>
2021-03-05 14:19:51 -0800

    removing bias as a state

--- accelerometer.cc
+++ accelerometer.cc
@@ -13,10 +13,10 @@ void Accelerometer::InitAccelerometer(MatrixXd *R_bn, VectorXd *w_bn, VectorXd *
 VectorXd Accelerometer::RunModel(VectorXd x) {
     VectorXd y(3);
     VectorXd g_n(3);
-    VectorXd b_a = GetSubspace(x, 0, 2);
+    // VectorXd b_a = GetSubspace(x, 0, 2);
     // VectorXd b_a = VectorXd::Zero(3);
     g_n << 0, 0, 9.81;
-    y = RotateVector(x.head(3))*(*_R_bn)*(-1*g_n) + b_a;
+    y = RotateVector(x.head(3))*(*_R_bn)*(-1*g_n) + (*_b_a);
     // y = RotateVector(x.head(3))*(*_R_bn)*RotateVector((*_dt)*(*_w_bn-x.tail(3)))*(-1*g_n);
     return y;
 }
